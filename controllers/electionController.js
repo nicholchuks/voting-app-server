49d0779e3841100e0e1ca31e83541cb7c3a13695
Nicholas chuks <niccobery91@gmail.com>
2025-01-29 22:28:29 +0100

    updated the election in the database with postmon

--- controllers/electionController.js
+++ controllers/electionController.js
@@ -4,6 +4,8 @@ const cloudinary = require("../utils/cloudinary");
 const path = require("path");
 
 const ElectionModel = require("../models/electionModel");
+const CandidateModel = require("../models/candidateModel");
+
 require("dotenv").config();
 
 // ......................ADD NEW ELECTION.....................
@@ -99,21 +101,87 @@ const getElection = async (req, res, next) => {
 // GET : api/elections/:id/candidates
 // PROTECTED
 const getCandidateOfElection = async (req, res, next) => {
-  res.json("Get Candidates of Election");
+  try {
+    const { id } = req.params;
+    const candidates = await CandidateModel.find({ election: id });
+    res.status(200).json(candidates);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ......................GET VOTER of ELECTION.....................
 // GET : api/elections/:id/voters
 // PROTECTED
 const getElectionVoters = async (req, res, next) => {
-  res.json("Get Election Voters");
+  try {
+    const { id } = req.params;
+    const response = await ElectionModel.findById(id).populate("voters");
+    res.status(200).json(response.voters);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ......................UPDATE ELECTION.....................
 // PATCH : api/elections/:id
 // PROTECTED (only admin)
 const updateElection = async (req, res, next) => {
-  res.json("Edit Election");
+  try {
+    //Only admin can add election
+    //   if(!req.user.isAdmin) {
+    //    return next(new HttpError("Only an admin can perform this action.", 403))
+    //  }
+    const { id } = req.params;
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return next(new HttpError("Fill in all fields.", 422));
+    }
+    if (req.files.thumbnail) {
+      const { thumbnail } = req.files; //image size should be less than 1mb
+      if (thumbnail.size > 1000000) {
+        return next(
+          new HttpError("Image size too big. Should be less than 1mb", 422)
+        );
+      }
+      let fileName = thumbnail.name;
+      fileName = fileName.split(".");
+      fileName = fileName[0] + uuid() + "." + fileName[fileName.length - 1];
+
+      // Upload file to upload folder
+      thumbnail.mv(
+        path.join(__dirname, "..", "uploads", fileName),
+        async (err) => {
+          if (err) {
+            return next(HttpError(err));
+          }
+
+          //  store image on cloudinary
+          const result = await cloudinary.uploader.upload(
+            path.join(__dirname, "..", "uploads", fileName),
+            { resource_type: "image" }
+          );
+
+          //check if cloudinary storage was successful
+          if (!result.secure_url) {
+            return next(
+              new HttpError(
+                "Image upload to cloudinary was not successful",
+                422
+              )
+            );
+          }
+          //Save election to db
+          await ElectionModel.findByIdAndUpdate(id, {
+            title,
+            description,
+            thumbnail: result.secure_url,
+          });
+          res.json("Election updated successfully");
+        }
+      );
+    }
+  } catch (error) {}
 };
 
 // ......................DELETE ELECTION.....................
